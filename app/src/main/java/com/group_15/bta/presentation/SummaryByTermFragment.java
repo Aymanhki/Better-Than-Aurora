caa19efbc34f335054d53c0eabe2552327a29d75
DESKTOP-EQQK3JN\Nilin Reza <nilin.reza@gmail.com>
2022-04-01 06:36:20 -0500

    complete implementation of Account Summary page with total balance due etc.

--- app/src/main/java/com/group_15/bta/presentation/SummaryByTermFragment.java
+++ app/src/main/java/com/group_15/bta/presentation/SummaryByTermFragment.java
@@ -10,11 +10,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.group_15.bta.R;
 import com.group_15.bta.business.AccessUsers;
+import com.group_15.bta.objects.Section;
 import com.group_15.bta.objects.Student;
+import com.group_15.bta.objects.StudentSection;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,11 +34,8 @@ public class SummaryByTermFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-<<<<<<< HEAD
     private Student currentUser = (Student) new AccessUsers().getCurrentUser();
-=======
-
->>>>>>> origin/main
+    private double tuition=0.0;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -63,12 +67,8 @@ public class SummaryByTermFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ((StudentAccountActivity) getActivity()).setActionBarTitle("Account Summary");
-<<<<<<< HEAD
-        String course="";
-        currentUser.g
-        Log.d("got until courseName",course);
-=======
->>>>>>> origin/main
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -81,7 +81,28 @@ public class SummaryByTermFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary_by_term, container, false);
+
+        View root=inflater.inflate(R.layout.fragment_summary_by_term, container, false);
+        TextView tuition_fees=(TextView)root.findViewById(R.id.tuition_fee_amount);
+        ArrayList<StudentSection> studentCurrentSections = currentUser.getStudentSections(true);
+        Log.d("number of courses",""+studentCurrentSections.size());
+        for (int i = 0; i <studentCurrentSections.size() ; i++) {
+            tuition+=studentCurrentSections.get(i).getAssociatedCourse().getTuition();
+
+            Log.d("course"+i,studentCurrentSections.get(i).getAssociatedCourse().getTitle()+tuition);
+        }
+        tuition_fees.setText(String.format("\n$%s", tuition));
+        TextView total_fees=(TextView) root.findViewById(R.id.total_fee_amount);
+        TextView total_payable=(TextView) root.findViewById(R.id.total_payable_amount);
+        double endowment_fee=39.00;
+        double library_fee=24.57;
+        double sport_fee=93.26;
+        double tech_fee=80.00;
+        double term_charges=tuition+endowment_fee+library_fee+sport_fee+tech_fee;
+        total_fees.setText(String.format("$%s\n",term_charges));
+        total_payable.setText(String.format("\n$%s\n",term_charges));
+        return root;
+
 
 
 
